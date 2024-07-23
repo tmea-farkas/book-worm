9a76dce92f6ac4c0e4b8f2112a4e5c22e3f549bf
Timea Farkas <tmeafarkas@yahoo.com>
2024-07-23 19:10:21 +0000

    created the post model

--- bookworm/models.py
+++ bookworm/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+STATUS = ((0, 'Draft'), (1, 'Posted'))
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    author = models.ForeignKey(#one-to-many foreign key
+        User, on_delete=models.CASCADE, related_name='bookworm_posts'
+    )
+    content = models.TextField() # post content
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)

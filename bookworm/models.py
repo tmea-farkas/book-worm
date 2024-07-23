d61441847abeff9d3141d6019e05fd4b6d754a53
Timea Farkas <tmeafarkas@yahoo.com>
2024-07-23 20:53:20 +0000

    begin creating the profile model

--- bookworm/models.py
+++ bookworm/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+import uuid
+from cloudinary.models import CloudinaryField
 # Create your models here.
 STATUS = ((0, 'Draft'), (1, 'Posted'))
 
@@ -11,4 +13,30 @@ class Post(models.Model):
     )
     content = models.TextField() # post content
     created_on = models.DateTimeField(auto_now_add=True)
+    edited_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    exerpt = models.TextField(blank=True)
+
+#class Profile(models.Model):
+ #   id = models.UUIDField(unique=True,
+ #                         primary_key=True,
+  #                        editable=False,
+   #                       default=uuid.uuid4)
+   # user = models.ForeignKey(User,
+    #                         blank=False,
+     #                        null=False,
+      #                       related_name='profile',
+       #                      on_delete=models.CASCADE)
+    #profile_picture = CloudinaryField(
+     #   'image',
+      #  default=(#insert cloudinary default image)
+    #),
+     #   store='#folder where profile images are stored#' ,
+      #  allowed_formats=['jpg', 'jpeg', 'png', 'webp'],
+       # display={'width':120,
+        #         'height':180,
+         #        'format': 'png',
+          #       'crop': 'fill',
+           #      'quality': 'auto:good'},
+        #bio = models.TextField()
+    #)

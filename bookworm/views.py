de702553b2af72215d999b3dbad70a0da5d73a9a
Timea Farkas <tmeafarkas@yahoo.com>
2024-07-27 17:10:09 +0000

    added custom font and background colour to base.html

--- bookworm/views.py
+++ bookworm/views.py
@@ -7,6 +7,10 @@ from .forms import NewUserForm, ProfileForm, UserUpdateForm, ProfileUpdateForm
 
 @login_required
 def profile_update(request):
+    user = request.user
+    if not hasattr(user, 'profile'):
+        Profile.objects.create(user=user)
+
     if request.method == 'POST':
         user_form = UserUpdateForm(request.POST, instance=request.user)
         profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
